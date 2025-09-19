f6eee2a6c06b4513869966dc7d2361b324fb1d17
laoneya <lavanyareddy10435@gmail.com>
2025-09-19 10:24:05 +0530

    changed localhost to vercel instance

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://dry-af.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
